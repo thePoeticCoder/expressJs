faae9cc03ed8a1fcbbccc9170d41d515fbdc9142
thePoeticCoder <parasjainn08@gmail.com>
2023-08-21 14:54:55 +0530

    pro imse,all

--- promiseAll.js
+++ promiseAll.js
@@ -1,10 +1,38 @@
-const promise1 = Promise.resolve(3);
-const promise2 = 42;
-const promise3 = new Promise((resolve, reject) => {
-  setTimeout(resolve, 100, 'foo');
+// const promise1 = Promise.resolve(3);
+// const promise2 = 42;
+// const promise3 = new Promise((resolve, reject) => {
+//   setTimeout(resolve, 100, 'foo');
+// });
+
+// Promise.all([promise1, promise2, promise3]).then((values) => {
+//   console.log(values);
+// });
+// Expected output: Array [3, 42, "foo"]
+
+console.time();
+const p1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log('The first promise has resolved');
+    resolve(10);
+  }, 1 * 1000);
+});
+const p2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log('The second promise has resolved');
+    resolve(20);
+  }, 1 * 1000);
+});
+const p3 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    console.log('The third promise has resolved');
+    resolve(30);
+  }, 4 * 1000);
 });
 
-Promise.all([promise1, promise2, promise3]).then((values) => {
-  console.log(values);
+Promise.all([p1, p2, p3]).then((results) => {
+  const total = results.reduce((p, c) => p + c);
+
+  console.log(`Results: ${results}`);
+  console.log(`Total: ${total}`);
+  console.timeEnd();
 });
-// Expected output: Array [3, 42, "foo"]
