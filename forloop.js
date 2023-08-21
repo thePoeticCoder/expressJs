1250028630323d6579d716d55326b7cc93999586
thePoeticCoder <parasjainn08@gmail.com>
2023-08-21 16:30:02 +0530

    let var const

--- forloop.js
+++ forloop.js
@@ -1,18 +1,27 @@
-let number =23;
-let flag =true;
-for(let div =2; div<number ;div++)
-{
-    if(number %div ==0)
-    {
-        flag =false;
-        break;
-    }
-}
-if(flag==true)
-{
-    console.log(number , "is prime");
+// let number =23;
+// let flag =true;
+// for(let div =2; div<number ;div++)
+// {
+
+//     if(number %div ==0)
+//     {
+//         flag =false;
+//         break;
+//     }
+// }
+// if(flag==true)
+// {
+//     console.log(number , "is prime");
 
 
-}else {
-    console.log(number , " is  not prime ");
+// }else {
+//     console.log(number , " is  not prime ");
+// }
+ 
+function help (){
+for (var i =0 ;i <3; i++){
 }
+
+}
+
+console.log(i+1);
--- test.js
+++ test.js
@@ -1,1 +1,22 @@
-console.log("hello js");
+
+const P1 =  Promise.resolve ("first promise");
+const P2 = 30;
+const P3 = new Promise((resolve,reject) => {
+	const a =2;
+	if(a>1){
+		resolve ("nice hero");
+	}else {
+		reject ("bfjsadgfdsa");
+	}
+
+})
+
+const P4 = new Promise ((resolve , reject)=>{
+
+	setTimeout(resolve,1000,'Success');
+
+})
+
+Promise.all([P1,P2,P3,P4]).then((values) => {
+	console.log(values);
+})
