d685b6b5d7ce6079040a21f3e4e2a9d608421eb2
thePoeticCoder <parasjainn08@gmail.com>
2024-01-15 10:16:20 +0530

    nested map

---
+++ object/nestedMap.js
@@ -0,0 +1,29 @@
+let game1 = {
+  teamLineup: [
+    {
+      team: {
+        actual: {
+          starters: [
+            {
+              player: { name: "Joe" },
+              player: { name: "Kim" },
+            },
+          ],
+        },
+      },
+    },
+    {
+      team: {
+        actual: {
+          starters: [
+            {
+              player: { name: "John" },
+              player: { name: "Shauna" },
+            },
+          ],
+        },
+      },
+    },
+  ],
+};
+//find all the starting players
